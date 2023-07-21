c1cef96b3ee43720c58af50d1f7771a7d3db5a60
Jude Alistair <jude55593@gmail.com>
2023-07-21 10:36:40 +0100

    sorting algos updated

--- sort.h
+++ sort.h
@@ -2,13 +2,21 @@
 #define SORT_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-/* Doubly linked list node structure */
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* Function prototypes for sorting algorithms */
@@ -21,4 +29,3 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 #endif /* SORT_H */
-
